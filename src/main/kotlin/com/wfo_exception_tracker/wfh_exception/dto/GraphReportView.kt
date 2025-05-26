7da717ee15a3514a068e57e3fb35afce347eb592
thasniya2000 <thasniyakply@gmail.com>
2025-05-26 15:40:34 +0530

    Committed after the deletion of unecessary dtos

--- src/main/kotlin/com/wfo_exception_tracker/wfh_exception/dto/GraphReportView.kt
+++
@@ -1,9 +0,0 @@
-package com.wfo_exception_tracker.wfh_exception.dto
-
-interface GraphReportView {
-    fun getYear(): Int
-    fun getPeriodValue(): Int
-    fun getTeam(): String
-    fun getStatus(): String     // Add this
-    fun getRequestCount(): Int
-}
